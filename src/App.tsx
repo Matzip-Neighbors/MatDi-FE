a9aca9b9a07e266b9efa06e586e4032769f52f01
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-02-28 17:52:56 +0900

    Feat: Get Authorization Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+
+const CLIENT_ID = "7e089ce9768f1024ea0759453ecf5460";
+const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <h1>
+              <a href={KAKAO_AUTH_URL}>Login</a>
+            </h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
