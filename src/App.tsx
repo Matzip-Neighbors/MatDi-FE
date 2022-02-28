7cca497b245343b29f35ae97ae9fdd32e66c0780
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-02-28 18:26:25 +0900

    Fix: Modifying Redirect URI

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Auth from "./Auth";
 import Profile from "./Profile";
 
 function App() {
-  const CLIENT_ID = process.env.KAKAO_API_KEY;
-  const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+  const CLIENT_ID = "c9b2b5ca6683c25e25d1269640cdb324";
+  const REDIRECT_URI = "http://localhost:3000/api/user/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return (
