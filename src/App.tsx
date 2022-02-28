0cfb633ff991826c189d7b10c6fdbb1460c38a65
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-02-28 22:43:15 +0900

    Fix:  Delete unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Auth from "./Auth";
 import Profile from "./Profile";
 
 function App() {
-  const CLIENT_ID = "c9b2b5ca6683c25e25d1269640cdb324";
+  const CLIENT_ID = process.env.REACT_APP_KAKAO_API_KEY;
   const REDIRECT_URI = "http://localhost:3000/api/user/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
