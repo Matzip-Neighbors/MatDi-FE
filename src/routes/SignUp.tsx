a64e51ce74b0534b59020fb3354dcc0918124dce
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-20 15:43:33 +0900

    Feat: Signup Form customizing

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -5,15 +5,18 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin-top: 50%;
-  padding: 0 2rem;
+  margin-top: 25%;
+  padding: 0 5rem;
   input {
     border-left-width: 0;
     border-right-width: 0;
     border-top-width: 0;
     border-bottom-width: 1;
     border-bottom: 1px solid #ccc;
-    margin-bottom: 1.5rem;
+    margin-bottom: 2rem;
+    &:focus {
+      outline: none;
+    }
   }
 `;
 
@@ -25,41 +28,41 @@ const Button = styled.button`
   background-color: skyblue;
   border: none;
   border-radius: 1.2rem;
+  cursor: pointer;
 `;
 
-const SignUp = (e: any) => {
+const SignUp = () => {
   const { register } = useForm();
 
   return (
     <Form>
       <input
-        {...register("이메일")}
+        {...register("이메일", { required: "이메일은 필수 항목입니다." })}
         type="email"
         placeholder="이메일"
-        required
       />
-      <input {...register("인증번호")} type="number" placeholder="인증번호" />
       <input
-        {...register("비밀번호")}
+        {...register("인증번호", { required: "인증번호는 필수 항목입니다." })}
+        type="number"
+        placeholder="인증번호"
+      />
+      <input
+        {...register("비밀번호", { required: "비밀번호는 필수 항목입니다." })}
         type="password"
         placeholder="비밀번호"
         required
       />
       <input
-        {...register("비밀번호 확인")}
+        {...register("비밀번호 확인", { required: "필수 항목입니다." })}
         type="password"
         placeholder="비밀번호 확인"
       />
-      <input {...register("닉네임")} type="text" placeholder="닉네임" />
+      <input
+        {...register("닉네임", { required: "닉네임은 필수 항목입니다." })}
+        type="text"
+        placeholder="닉네임"
+      />
       <Button>회원가입</Button>
-      {/* <Form>
-        <input type="email" placeholder="이메일" />
-        <input type="number" placeholder="인증번호" />
-        <input type="password" placeholder="비밀번호" />
-        <input type="password" placeholder="비밀번호 확인" />
-        <input type="password" placeholder="비밀번호 확인" />
-        <Button>회원가입</Button>
-      </Form> */}
     </Form>
   );
 };
