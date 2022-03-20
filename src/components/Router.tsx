fe48100c6edf73799ff4d1ae031b172047b173d8
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-20 15:21:43 +0900

    Feat: KakaoURl deleted, Readme update

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,10 +3,6 @@ import Login from "./../routes/Login";
 import SignUp from "./../routes/SignUp";
 
 const Router = () => {
-  const CLIENT_ID = process.env.REACT_APP_KAKAO_API_KEY;
-  const REDIRECT_URI = "http://localhost:3000/api/user/kakao/callback";
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
   return (
     <BrowserRouter>
       <div className="App">
