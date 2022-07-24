b9669b93e4e971ab950237cc6257fe0186a7c118
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-07-24 22:06:00 +0900

    Feat: Material UI 및 react-hook-form validation 적용

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,73 +1,118 @@
-import { useForm } from "react-hook-form";
-import styled from "styled-components";
-import PageTitle from "./../components/PageTitle";
+import { Container, makeStyles, Typography, TextField, Button } from '@material-ui/core';
+import { useState } from 'react';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin-top: 25%;
-  padding: 0 5rem;
-  input {
-    border-left-width: 0;
-    border-right-width: 0;
-    border-top-width: 0;
-    border-bottom-width: 1;
-    border-bottom: 1px solid #ccc;
-    margin-bottom: 2rem;
-    &:focus {
-      outline: none;
-    }
-  }
-`;
+interface SignUpForm {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  authCode: string;
+  nickname: string;
+}
 
-const Button = styled.button`
-  display: flex;
-  margin: 0 auto;
-  padding: 1rem 3rem;
-  font-size: 18px;
-  background-color: skyblue;
-  border: none;
-  border-radius: 1.2rem;
-  cursor: pointer;
-`;
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .max(255)
+    .required('Email is required')
+    .matches(/^([A-Za-z]|[0-9])+$/, 'Email is invalid'),
+  password: yup.string().required('Password is required').min(8).max(120),
+  confirmPassword: yup
+    .string()
+    .required('Password is required')
+    .oneOf([yup.ref('password'), null], 'Passwords must match'),
+  nickname: yup.string().required().min(2).max(25),
+});
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    textAlign: 'center',
+    margin: theme.spacing(1, 0, 4),
+  },
+  submitButton: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 const SignUp = () => {
-  const { register } = useForm();
+  const { heading, submitButton } = useStyles();
+  const [json, setJson] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpForm>({ resolver: yupResolver(schema) });
+
+  const onValid = (data: SignUpForm) => {
+    setJson(JSON.stringify(data, null, 2));
+  };
 
   return (
-    <>
-      <PageTitle title="Sign Up" />
-      <Form>
-        <input
-          {...register("이메일", { required: "이메일은 필수 항목입니다." })}
-          type="email"
-          placeholder="이메일"
-        />
-        <input
-          {...register("인증번호", { required: "인증번호는 필수 항목입니다." })}
-          type="number"
-          placeholder="인증번호"
-        />
-        <input
-          {...register("비밀번호", { required: "비밀번호는 필수 항목입니다." })}
-          type="password"
-          placeholder="비밀번호"
+    <Container maxWidth="xs">
+      <Typography className={heading} variant="h4">
+        Create Account
+      </Typography>
+      <form onSubmit={handleSubmit(onValid)}>
+        <TextField
+          {...register('email')}
+          variant="outlined"
+          margin="normal"
+          label="email"
+          fullWidth
           required
+          error={!!errors.email?.message}
+          helperText={errors.email?.message}
         />
-        <input
-          {...register("비밀번호 확인", { required: "필수 항목입니다." })}
+        <TextField
+          {...register('password')}
+          variant="outlined"
+          margin="normal"
+          label="password"
           type="password"
-          placeholder="비밀번호 확인"
+          fullWidth
+          required
+          error={!!errors.password}
+          helperText={errors.password?.message}
         />
-        <input
-          {...register("닉네임", { required: "닉네임은 필수 항목입니다." })}
-          type="text"
-          placeholder="닉네임"
+        <TextField
+          {...register('confirmPassword')}
+          variant="outlined"
+          margin="normal"
+          type="password"
+          label="confirmPassword"
+          fullWidth
+          required
+          error={!!errors.confirmPassword?.message}
+          helperText={errors.confirmPassword?.message}
         />
-        <Button>회원가입</Button>
-      </Form>
-    </>
+        <TextField
+          {...register('authCode')}
+          variant="outlined"
+          margin="normal"
+          label="authCode"
+          fullWidth
+          required
+          error={!!errors.authCode?.message}
+          helperText={errors.authCode?.message}
+        />
+        <TextField
+          {...register('nickname')}
+          variant="outlined"
+          margin="normal"
+          label="nickname"
+          fullWidth
+          required
+          error={!!errors.nickname}
+          helperText={errors.nickname?.message}
+        />
+        <Button type="submit" fullWidth variant="contained" color="primary" className={submitButton}>
+          Sign Up
+        </Button>
+      </form>
+    </Container>
   );
 };
 
