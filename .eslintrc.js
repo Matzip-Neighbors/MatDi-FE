2ec298ac8eb72b495f2b8ae26b6acc82e1a58979
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-07-24 12:50:53 +0900

    Feat: Parsing Error 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parserOptions: {
-        project: './tsconfig.js',
-    },
-    rules: {
-        '@typescript-eslint/semi': 'off',
-        '@typescript-eslint/space-before-function-paren': 'off',
-    },
-    ignorePatterns: ['.eslintrc.js'],
-    env: {
-        browser: true,
-    },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  env: {
+    browser: true,
+  },
 };
